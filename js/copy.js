cd9a50ab885a1e6e06015edaff570de7a9b78a24
lozenge0714 <1310849396@qq.com>
2023-02-17 23:27:52 +0800

    Site updated: 2023-02-17 23:27:52

---
+++ js/copy.js
@@ -0,0 +1,18 @@
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "复制成功🦉",
+                    message: "转载请保留原文链接，感谢你的喜欢！",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
